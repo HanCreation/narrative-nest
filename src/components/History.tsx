500a5764150e332fb85e970c0201fb9df5774124
Han <coc.hans@yahoo.com>
2024-05-23 18:46:11 +0700

    Minor Adjust
    For History and Storyboard title

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
-import Header2 from './Header2';
 import Footer from './Footer';
 import HeaderStory from './HeaderStory';
 const HistoryPage: React.FC = () => {
--- src/components/Storyboard.tsx
+++ src/components/Storyboard.tsx
@@ -1,35 +1,8 @@
-// import React, { useState } from 'react';
-// import ImageCard from './ImageCard';
-
-// const Storyboard: React.FC = () => {
-//   const [basePrompt, setBasePrompt] = useState("");
-
-//   return (
-//     <div className="p-4 flex flex-col items-center w-full">
-//       <div className="w-full max-w-screen-lg mb-4">
-//         <input
-//           type="text"
-//           value={basePrompt}
-//           onChange={(e) => setBasePrompt(e.target.value)}
-//           placeholder="Enter base prompt"
-//           className="w-full p-2 border rounded mb-2"
-//         />
-//       </div>
-//       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-items-center w-full max-w-screen-lg">
-//         {Array(9).fill(null).map((_, index) => (
-//           <ImageCard key={index} basePrompt={basePrompt} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Storyboard;
 import React, { useState } from 'react';
 import Header from './Header.tsx';
 import ImageCard from './ImageCard';
 import Footer from './Footer.tsx';
-
+import { Helmet } from 'react-helmet';
 const Storyboard: React.FC = () => {
   const [storyboardTitle, setStoryboardTitle] = useState("UNNAMED STORYBOARD");
   const [basePrompt, setBasePrompt] = useState("");
@@ -40,6 +13,9 @@ const Storyboard: React.FC = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url('/Background.png')`, backdropFilter: 'blur(10px)' }}>
+      <Helmet>
+        <title>Storyboard - Narrative Nest</title>
+      </Helmet>
       <Header initialTitle={storyboardTitle} onTitleChange={handleTitleChange} />
       <div className="flex-grow p-4">
         <div className="p-4 flex flex-col items-center w-full">
