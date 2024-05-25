d014d58c7acf1a92643c7957a6bbe02b7c08d502
Hndra04 <109862480+Hndra04@users.noreply.github.com>
2024-05-25 18:47:51 +0700

    Adding Documentation on Header3.tsx

--- src/components/Header3.tsx
+++ src/components/Header3.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// Importing react library
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
+  // Returning Header3
   return (
+    // Navigation Bar
     <header className="p-4 bg-glass2 shadow-md flex items-center relative">
       <div className="absolute left-4">
+        {/* Link to Home Page */}
         <Link to="/">
           <img src="/Logo.png" alt="Narrative Nest Logo" className="h-8" />
         </Link>
@@ -12,9 +16,10 @@ const Header: React.FC = () => {
       <div className="mx-auto text-xl font-bold text-center text-brown-600">
         NARRATIVE NEST
       </div>
-
     </header>
+    // Navigation Bar End
   );
 };
 
+// Exporting Header3 Page
 export default Header;
