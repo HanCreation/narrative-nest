467d5949f4f77e272d39f84368514db33276f910
Hndra04 <109862480+Hndra04@users.noreply.github.com>
2024-05-25 18:48:14 +0700

    Adding Documentation on HeaderStory.tsx

--- src/components/HeaderStory.tsx
+++ src/components/HeaderStory.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// Importing necessary page and react library
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HeaderVariant3: React.FC = () => {
+  // Return Header Story Page
   return (
+    // Navigation Bar
     <header className="p-4 bg-glass2 shadow-md flex items-center relative">
       <div className="absolute left-4">
+        {/* Link to Homepage */}
         <Link to="/">
           <img src="/Logo.png" alt="Narrative Nest Logo" className="h-8" />
         </Link>
@@ -13,13 +17,21 @@ const HeaderVariant3: React.FC = () => {
         NARRATIVE NEST
       </div>
       <div className="absolute right-4 flex items-center space-x-4">
-        <Link to="/storyboard" className="bg-brown-500 text-white px-4 py-2 rounded hover:bg-brown-600">
+
+        {/* Link to Storyboard Maker Page */}
+        <Link
+          to="/storyboard"
+          className="bg-brown-500 text-white px-4 py-2 rounded hover:bg-brown-600"
+        >
           New Storyboard
         </Link>
         <div className="h-8 w-8 bg-gray-300 rounded-full"></div>
+
       </div>
     </header>
+    // Navigation Bar End
   );
 };
 
+// Exporting Header Story Page
 export default HeaderVariant3;
