da35a3ac980cf0fb35352792ab7e9ce7c1e0729a
Hndra04 <109862480+Hndra04@users.noreply.github.com>
2024-05-25 18:45:05 +0700

    Adding Documentation to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Importing necessary page and react library
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
@@ -7,13 +8,18 @@ import History from './components/History';
 // import Create from './components/Create';
 import Storyboard from './components/Storyboard';
 
+
 const App: React.FC = () => {
+  // Returning App Page
   return (
+    // ROUTER: wraps the entire application. It enables the use of React Router for managing navigation within the app.
     <Router>
+      {/* ROUTES: container for all the Route components. It defines how different URLs in the application map to different components. */}
       <Routes>
+        {/*ROUTE: specifies the component to render when the URL matches the path. */}
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />  
+        <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/history" element={<History />} />
         {/* <Route path="/create" element={<Create />} /> */}
         <Route path="/storyboard" element={<Storyboard />} />
@@ -22,4 +28,5 @@ const App: React.FC = () => {
   );
 };
 
+// Exporting App page
 export default App;
