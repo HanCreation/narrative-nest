f7a3495246e2cdcaac9c5d6b4c8c5fc97b6862bd
Hndra04 <109862480+Hndra04@users.noreply.github.com>
2024-05-25 18:47:32 +0700

    Adding Documentation on Header2.tsx

--- src/components/Header2.tsx
+++ src/components/Header2.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// Importing necessary react library
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
+  // Returning Header2 Page
   return (
+    // Navigation bar (Navbar)
     <header className="p-4 bg-glass2 shadow-md flex items-center relative">
       <div className="absolute left-4">
         <Link to="/">
@@ -13,11 +16,29 @@ const Header: React.FC = () => {
         NARRATIVE NEST
       </div>
       <nav className="absolute right-4 flex items-center space-x-4">
-        <Link to="/login" className="text-brown-600 hover:text-brown-800 font-semibold">Login</Link>
-        <Link to="/sign-up" className="bg-brown-500 text-white px-3 py-2 rounded hover:bg-brown-600">Get Started</Link>
+        
+        
+        {/* Link Home page to Login Page */}
+        <Link
+          to="/login"
+          className="text-brown-600 hover:text-brown-800 font-semibold"
+        >
+          Login
+        </Link>
+
+
+        {/* Link Home page to Sign Up Page */}
+        <Link
+          to="/sign-up"
+          className="bg-brown-500 text-white px-3 py-2 rounded hover:bg-brown-600"
+        >
+          Get Started
+        </Link>
       </nav>
     </header>
+    // Navbar End
   );
 };
 
+// Exporting Header2 Page
 export default Header;
